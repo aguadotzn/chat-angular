624239e285cea492e9c4421a204b03ccccc64057
AdrianAg <adrianaguado@protonmail.com>
2021-02-02 19:31:32 +0100

    feature: add pipe [3th class]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages/messages.component';
 import { TextButtonComponent } from './text-button/text-button.component';
+import { MimimiPipe } from './pipes/mimimi.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessagesComponent,
-    TextButtonComponent
+    TextButtonComponent,
+    MimimiPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/mimimi.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mimimi'
+})
+export class MimimiPipe implements PipeTransform {
+
+  transform(value: string, ...args: unknown[]): unknown {
+    const st = value.replace(/[aeiou]/gi, 'i');
+    return st;
+  }
+}
