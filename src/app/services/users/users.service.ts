70d9f0499e3c428083034d9e338f581c0f33a3c2
AdrianAg <adrianaguado@protonmail.com>
2021-02-08 18:43:23 +0100

    refactor: stories styles and other stuff [3th class]

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -3,15 +3,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UsersService {
   private baseUrl = 'https://randomuser.me/api/';
 
   constructor(private httpClient: HttpClient) { }
 
   public getUsers(): Observable<User> {
-    return this.httpClient.get(`${this.baseUrl}?results=5`) as Observable<User>;
+    return this.httpClient.get(`${this.baseUrl}?results=6`) as Observable<User>;
   }
 }
