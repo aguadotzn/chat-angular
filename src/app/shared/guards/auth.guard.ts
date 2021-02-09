269b0c8ea3095e5d87fb31e25ce9c5d8e00acbe9
AdrianAg <adrianaguado@protonmail.com>
2021-02-09 20:44:43 +0100

    feature: add auth guard [5th class]

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const isLogged = window.localStorage.getItem('user') != null;
+    if (!isLogged) {
+      alert('⚠️ Necesitas estar logueado!');
+    }
+    return isLogged;
+  }  
+}
